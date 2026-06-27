020730e139e6b848bc2f97d5164f3360cd8891a7
Nancy Ahmed <nancyahmed9393@gmail.com>
2026-06-27 18:36:19 +0300

    fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/rctws_neamat1/',
   build: {
     rollupOptions: {
       output: {
